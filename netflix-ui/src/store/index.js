2127d276ea5d292de93fa1b891b17c218202874b
PavanMacharla <macharlapavan440@gmail.com>
2023-06-23 10:25:35 +0530

    ui test

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -81,7 +81,7 @@ export const getUsersLikedMovies = createAsyncThunk(
   async (email) => {
     const {
       data: { movies },
-    } = await axios.get(`http://localhost:6000/api/user/liked/${email}`);
+    } = await axios.get(`https://netflix-node-bt78.vercel.app/api/user/liked/${email}`);
     return movies;
   }
 );
@@ -91,7 +91,7 @@ export const removeMovieFromLiked = createAsyncThunk(
   async ({ movieId, email }) => {
     const {
       data: { movies },
-    } = await axios.put("http://localhost:6000/api/user/remove", {
+    } = await axios.put("https://netflix-node-bt78.vercel.app/api/user/remove", {
       email,
       movieId,
     });
